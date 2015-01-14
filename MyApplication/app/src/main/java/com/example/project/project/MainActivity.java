36e3a5a564da0f6e76a037816f7c9945d7e51720
lixXxor <gzaphf@Hans.local>
2015-01-14 16:31:40 +0100

    fungerar sådär med addering av note
    dsa

--- MyApplication/app/src/main/java/com/example/project/project/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/project/project/MainActivity.java
@@ -3,106 +3,123 @@ package com.example.project.project;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import android.app.Activity;
 
+import com.terlici.dragndroplist.DragNDropSimpleAdapter;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+
+import android.widget.AdapterView.OnItemClickListener;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
     /** Called when the activity is first created. */
 
+    private ListView lstView;
+    private EditText edtText;
+    private SimpleAdapter simpAdapter;
+    private myListAdapter listAdapter;
+    static List<String> list = new ArrayList<String>();
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_list_view);
+        setUpView();
+    }
 
-        SimpleAdapter adapter = new SimpleAdapter(
-                this,
-                list,
-                R.layout.custom_row_view,
-                new String[] {"note", "button"},
-                new int[] {R.id.text1, R.id.text4}
-        );
-        //how should I implement this to public void btnOnClick?
-        createNewNote();
-        populateList();
-        setListAdapter(adapter);
+    private void setUpView(){
+        lstView = (ListView)findViewById(R.id.listView_a);
+        edtText = (EditText)findViewById(R.id.text1);
+
+        list.clear();
+        //populateList();
+        listAdapter = new myListAdapter(this, R.layout.custom_row_view, list);
+        lstView.setAdapter(listAdapter);
+
+//        lstView.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView parent, View view,
+//                                    int position, long id) {
+//                String  U_id = ((EditText)view.findViewById(R.id.text1)).getText().toString();
+//                Log.d("hej", "hej");
+//            }
+//        });
+
+        lstView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String selected = edtText.getText().toString();
+
+                Toast toast=Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
 
     }
+
 
     //creating a new note button
     public void btnOnClick(View v){
         Button theButton = (Button)findViewById(R.id.add_note);
+        createNewNote();
         //theButton.setBackgroundColor(000);
         Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
     }
 
     //PETER Här ska du lägga till kod för att få upp datum fliken
     public void addDateButton(View v){
-        
         Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+
     }
 
-    static final ArrayList<HashMap<String,String>> list =
-            new ArrayList<HashMap<String,String>>();
-
-
     private void populateList() {
-        HashMap<String,String> temp = new HashMap<String,String>();
-        temp.put("note","MONT Blanc");
-        temp.put("button", "ADD DATE");
-        list.add(temp);
-        HashMap<String,String> temp1 = new HashMap<String,String>();
-        temp1.put("note","Gucci Gucci Gucci GucciGucciGucciGucciGucciGucciGucciGucci Gucci");
-        temp1.put("button", "ADD DATE");
-        list.add(temp1);
-        HashMap<String,String> temp2 = new HashMap<String,String>();
-        temp2.put("note","Parker");
-        temp2.put("button", "ADD DATE");
-        list.add(temp2);
-        HashMap<String,String> temp3 = new HashMap<String,String>();
-        temp3.put("note","Sailor");
-        temp3.put("button", "ADD DATE");
-        list.add(temp3);
-        HashMap<String,String> temp4 = new HashMap<String,String>();
-        temp4.put("note","Sailor Design");
-        temp4.put("button", "ADD DATE");
-        list.add(temp4);
-        HashMap<String,String> temp5 = new HashMap<String,String>();
-        temp5.put("note","Porsche SailorSailorSailorDesign");
-        temp5.put("button", "ADD DATE");
-        list.add(temp5);
-        HashMap<String,String> temp6 = new HashMap<String,String>();
-        temp6.put("note","SailorSailorSailorSailor Design");
-        temp6.put("button", "ADD DATE");
-        list.add(temp6);
-        HashMap<String,String> temp7 = new HashMap<String,String>();
-        temp7.put("note","Porsche Design");
-        temp7.put("button", "ADD DATE");
-        list.add(temp7);
-        HashMap<String,String> temp8 = new HashMap<String,String>();
-        temp8.put("note","Porsche SailorSailorSailorDesign");
-        temp8.put("button", "ADD DATE");
-        list.add(temp4);
-        HashMap<String,String> temp9 = new HashMap<String,String>();
-        temp9.put("note","Porsche Gucci GucciGucciGucciGucciGucc Gucci GucciGucciGucciGucciGuccGucci GucciGucciGucciGucciGuccDesign");
-        temp9.put("button", "ADD DATE");
-        list.add(temp9);
+
+        list.add("MONT Blanc");
+        list.add(("MONT ParkerParkerParkerParker"));
+        list.add(("ParkerParkerParkerParker Blanc"));
+        list.add(("SailorSailorSailorDesign SailorSailorSailorDesign"));
+        list.add(("GucciGGucciGucciGucciGucciGuccGucciGucciGucciGucciGuccucciGucciGucciGuccGucci Blanc"));
+        list.add(("MONT ParkerParkerParkerParker"));
+        list.add(("ParkerParkerParkerParker Blanc"));
+        list.add(("SailorSailorSailorDesign SailorSailorSailorDesign"));
 
     }
 
     private void createNewNote(){
-        HashMap<String,String> temp = new HashMap<String,String>();
-        temp.put("note","");
-        temp.put("button", "ADD DATE");
-        list.add(temp);
+        //want to add to the top
+
+        list.add(0,"");
+        listAdapter.notifyDataSetChanged();
+        //EditText theText = (EditText)findViewById(R.id.text1);
+        //theText.requestFocus();
+    }
+
+    private void removeNote(){
+
+        listAdapter.notifyDataSetChanged();
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/project/project/myListAdapter.java
@@ -0,0 +1,91 @@
+package com.example.project.project;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.v4.widget.ViewDragHelper;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+
+import java.util.List;
+
+/**
+ * Created by gzaphf on 14/01/15.
+ */
+public class myListAdapter extends ArrayAdapter<String>
+
+{
+
+    private List<String> myItems;
+    private int myResource;
+
+    public myListAdapter(Context context, int resource) {
+        super(context, resource);
+        myResource = resource;
+    }
+
+    public myListAdapter(Context context, int resource, List<String> items){
+        super(context, resource, items);
+        myItems = items;
+        myResource = resource;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent){
+
+        View v = convertView;
+
+
+
+        if(v == null){
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(myResource, null);
+
+        }
+
+        String p = myItems.get(position);
+        Button btnDeleteNote = (Button)v.findViewById(R.id.deleteBtn);
+
+        v.setId(position);
+
+        if(p != null){
+            EditText edt = (EditText)v.findViewById(R.id.text1);
+            if(position == 0)
+                edt.requestFocus();
+            
+            btnDeleteNote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("size myItems: ", " is " + myItems.size());
+                    remove(myItems.get(position));
+                    Log.d("size myItems: ", " is "+myItems.size());
+                }
+            });
+            edt.setText(p);
+            edt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus){
+                        final EditText editT = (EditText) v;
+                        myItems.set(position, editT.getText().toString());
+                    }
+                }
+            });
+
+        }
+
+        return v;
+    }
+
+
+
+}
